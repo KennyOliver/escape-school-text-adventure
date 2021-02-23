a99997ffa822cb72bb3bd487b9e3560e760d122c
Kenny <kenny_oliver@icloud.com>
2021-02-23 19:42:54 +0000

    [+] Better text UI, with more whitespace

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 from colorama import Fore, Back
 
-def adventure(money):
+def adventure(input_money):
+  wallet = input_money
+  
   # STAGE 1
   response = ""
   while response not in yes_no:
     response = input("Would you like to climb over the fence? yep/nope\n\t--> ").lower()
     if response == "yep":
-      print("You climb over the fence, with a few scrapes, but you managed to get out into the council estate.\n")
+      print("\nYou climb over the fence, with a few scrapes, but you managed to get out into the council estate.")
+      print(f"You found your wallet that someone threw over the fence last year!\nIt still has £{wallet}")
     elif response == "nope":
       print(f"You tried to get back inside the school, but a teacher caught you.\nGoodbye, {name}!")
       return
   
   # STAGE 2
+  print("A caretaker is walking towards the fence to see what the sound was!\n\nYou need to leave. QUICK!")
+  response = ""
+  print(Fore.RED, f"{left_txt}a path leading somewhere...")
+  print(Fore.LIGHTGREEN_EX, f"{fwd_txt}the council estate")
+  print(Fore.YELLOW, f"{rit_txt}a 10ft wooden fence")
+  print(Fore.CYAN, f"{bhd_txt}the school", Fore.WHITE)
+  while response not in directions:
+    response = input("What do you choose? (A/W/S/D)\n\t--> ").upper()
+  if response == 'A':
+    print(f"You get caught by a teacher on break duty.\nYOU LOSE, {name}!\n\nPS: They bought some candy with your money.")
+    return
+  elif response == 'W':
+    print("You head into the council estate.\n")
+    wallet += 10
+    see_wallet()
+  elif response == 'S':
+    print("It's too high.\n")
+    wallet -= 5
+    see_wallet()
+    response = "" #restarts if-statement
+  elif response == 'D':
+    print(f"You chose detention.\nSo long, {name}!")
+    return
+  
+  # STAGE 3
+  print("You see two roads ahead of you.")
   response = ""
   print(Fore.RED, f"{left_txt}a path leading somewhere...")
   print(Fore.LIGHTGREEN_EX, f"{fwd_txt}a small group of run-down bungalows")
@@ -35,31 +64,9 @@ def adventure(money):
   elif response == 'D':
     print(f"You leave the council estate. So long, {name}.")
     return
-  
-  # STAGE 3
-  print("You see two roads ahead")
-  response = ""
-  print(Fore.RED, f"{left_txt}a patho leading somewhere...")
-  print(Fore.LIGHTGREEN_EX, f"{fwd_txt}a small group of run-down bungalows")
-  print(Fore.YELLOW, f"{rit_txt}a 10ft wooden fence")
-  print(Fore.CYAN, f"{bhd_txt}the school")
-  print(Fore.WHITE, "\n")
-  while response not in directions:
-    response = input("What direction do you move? (A/W/S/D)\n\t--> ").upper()
-  if response == 'A':
-    print(f"You get caught by a teacher on break duty.\nYOU LOSE, {name}!")
-    return
-  elif response == 'W':
-    print("It's too high.\n")
-    response = ""
-  elif response == 'S':
-    print("You head deeper into the council estate.\n")
-  elif response == 'D':
-    print(f"You leave the council estate. So long, {name}.")
-    return
 #====================
-def get_money():
-  print(f"Wallet: £{money}")
+def see_wallet(amount):
+  print(f"Wallet: £{amount}")
 #====================
 # MAIN PROGRAM
 # Setup
@@ -69,7 +76,7 @@ left_txt = "A | Left:\t\t"
 fwd_txt = "W | Forward:\t"
 rit_txt = "S | Right:\t\t"
 bhd_txt = "D | Behind:\t"
-money = 0
+money = 1
 
 # Introduction
 print("<-- ESCAPE SCHOOL!!! -->")
