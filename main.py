c6b1c88755c33d57849b614f8338708cb0250a93
Kenny <kenny_oliver@icloud.com>
2021-02-23 17:42:47 +0000

    [+] Changed controls from ABCD to AWSD [+] Changed many strings

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from colorama import Fore, Back
+
 def adventure():
   # Setup
   yes_no = ["yep", "nope"]
-  directions = ['A','B','C','D']
+  directions = ['A','W','S','D']
   
   # Introduction
+  print("<-- ESCAPE SCHOOL!!! -->")
   name = input("What's your name?\n\t--> ")
   print(f"Hey, {name}! Let's escape school!")
   print("You find yourself on the edge of the field.")
@@ -12,7 +15,7 @@ def adventure():
   # Start of game
   response = ""
   while response not in yes_no:
-    response = input("Would you like to climb over the fence?\nyep/nope\n")
+    response = input("Would you like to climb over the fence?\nyep/nope\n").lower()
     if response == "yep":
       print("You climb over the fence, getting a few scrapes, but you managed to get out into the council estate.\n")
     elif response == "nope":
@@ -24,19 +27,19 @@ def adventure():
     # Next part of game
     response = ""
     while response not in directions:
-      print(Fore.RED, "A | To your left, you see a pathway leading somewhere...", Fore.WHITE)
-      print(Fore.YELLOW, "B | To your right, you see a small group of run-down bungalows.", Fore.WHITE)
-      print(Fore.LIGHTGREEN_EX, "C | There\'s a 10ft wooden fence directly in front of you.", Fore.WHITE)
-      print(Fore.CYAN, "D | Behind you is the school.\n", Fore.WHITE)
+      print(Fore.RED, "A | Left: a pathway leading somewhere...", Fore.WHITE)
+      print(Fore.LIGHTGREEN_EX, "W | Forward: a small group of run-down bungalows.", Fore.WHITE)
+      print(Fore.YELLOW, "S | Right: There\'s a 10ft wooden fence.", Fore.WHITE)
+      print(Fore.CYAN, "D | Behind: the school.\n", Fore.WHITE)
       response = input("What direction do you move?\nA/B/C/D\n")
       if response == 'A':
         print("You get caught by a teacher on break duty. YOU LOSE, " + name + "!")
         return
-      elif response == 'B':
-        print("You head deeper into the council estate.\n")
-        response = ""
-      elif response == 'C':
+      elif response == 'W':
         print("It's too high.\n")
+        response = ""
+      elif response == 'S':
+        print("You head deeper into the council estate.\n")
         response = ""
       elif response == 'D':
         print(f"You leave the council estate. So long, {name}.")
